cacc1601452e8ddfb9702b308c20df9ccf89d3f4
gar529 <Greg@gregrussellracing.com>
2018-06-07 19:01:46 -0400

    first commit

---
+++ GaugeProject/src/gauge_project/Main.java
@@ -0,0 +1,15 @@
+package gauge_project;
+
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Parser parser = new Parser();
+		parser.runParser();
+
+	}
+
+}
