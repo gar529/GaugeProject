4cd45265a8a540009a60a86c7b49644cf8166ec1
gar529 <Greg@gregrussellracing.com>
2018-08-18 13:31:45 -0400

    Revert "update 8/18 added settings, added favorite limit"
    This reverts commit 85ca2a4

--- app/src/main/java/com/gregrussell/fenwickguageapp/GaugeApplication.java
+++ app/src/main/java/com/gregrussell/fenwickguageapp/GaugeApplication.java
@@ -1,19 +1,29 @@
 package com.gregrussell.fenwickguageapp;
 
+import android.app.AlarmManager;
 import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.SQLException;
 import android.os.Build;
 import android.os.Environment;
+import android.os.SystemClock;
 import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Random;
 
 public class GaugeApplication extends Application {
 
     public static DataBaseHelperGauges myDBHelper;
+    public static final String CHANNEL_ID = "Flood Warning";
     public static final int GAUGES_ID_POSITION = 0;
     public static final int GAUGES_IDENTIFIER_POSITION = 1;
     public static final int GAUGES_NAME_POSITION = 2;
@@ -38,6 +48,8 @@ public class GaugeApplication extends Application {
     public static final int LOWER_BOUND_MILLIS = 1000 * 60 * 12;
     public static final int UPPER_BOUND_MILLIS = 1000 * 60 * 17;
 
+
+
     @Override
     public void onCreate() {
         super.onCreate();
@@ -56,6 +68,35 @@ public class GaugeApplication extends Application {
             throw sqle;
 
         }
+
+        logger();
+
+        ComponentName receiver = new ComponentName(this, StartAlarmAtBoot.class);
+        PackageManager pm = this.getPackageManager();
+
+        pm.setComponentEnabledSetting(receiver,
+                PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+                PackageManager.DONT_KILL_APP);
+
+        createNotificationChannel();
+
+        AlarmManager alarmMgr = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(this,AlarmReceiver.class);
+        PendingIntent alarmIntent = PendingIntent.getBroadcast(this,0,intent,0);
+        intent.setAction("com.gregrussell.alarmtest.SEND_BROADCAST");
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){
+            alarmMgr.setWindow(AlarmManager.ELAPSED_REALTIME, SystemClock.elapsedRealtime() + FIFTEEN_MINUTES_MILLIS, UPPER_BOUND_MILLIS - FIFTEEN_MINUTES_MILLIS, alarmIntent);
+        }else{
+            Random random = new Random();
+            int randomTimeMillis = random.nextInt(UPPER_BOUND_MILLIS - LOWER_BOUND_MILLIS) + LOWER_BOUND_MILLIS;
+            alarmMgr.set(AlarmManager.ELAPSED_REALTIME,SystemClock.elapsedRealtime() + randomTimeMillis,alarmIntent);
+        }
+
+
+    }
+
+    public void logger(){
 
         if ( isExternalStorageWritable() ) {
 
@@ -89,43 +130,6 @@ public class GaugeApplication extends Application {
             // not accessible
         }
 
-
-    }
-
-    public void logger(){
-
-        if ( isExternalStorageWritable() ) {
-
-            Log.d("logger","logging");
-
-            File appDirectory = new File( Environment.getExternalStorageDirectory() + "/GaugeProjectLogs" );
-            File logDirectory = new File( appDirectory + "/log" );
-            File logFile = new File( logDirectory, "logcat" + System.currentTimeMillis() + ".txt" );
-
-            // create app folder
-            if ( !appDirectory.exists() ) {
-                appDirectory.mkdir();
-            }
-
-            // create log folder
-            if ( !logDirectory.exists() ) {
-                logDirectory.mkdir();
-            }
-
-            // clear the previous logcat and then write the new one to the file
-            try {
-                Process process = Runtime.getRuntime().exec("logcat -c");
-                process = Runtime.getRuntime().exec("logcat -f " + logFile);
-            } catch ( IOException e ) {
-                e.printStackTrace();
-            }
-
-        } else if ( isExternalStorageReadable() ) {
-            // only readable
-        } else {
-            // not accessible
-        }
-
     }
 
     /* Checks if external storage is available for read and write */
@@ -145,16 +149,25 @@ public class GaugeApplication extends Application {
             return true;
         }
         return false;
+    }
 
-
-
-
-
-
-
-
-
-
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.enableLights(true);
+            channel.enableVibration(true);
+            channel.setDescription(description);
+            channel.setVibrationPattern(new long[]{1000, 1000, 1000});
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 
 
